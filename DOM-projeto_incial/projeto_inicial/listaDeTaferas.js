e4390c0cc39757ac108740444453e1c0b8861f7e
Kauã Silva <kauad4235@gmail.com>
2022-01-09 18:13:35 -0300

    botão de concluir criado.

--- DOM-projeto_incial/projeto_inicial/listaDeTaferas.js
+++ DOM-projeto_incial/projeto_inicial/listaDeTaferas.js
@@ -1,4 +1,5 @@
-const criarTarefa = (evento)=>{
+( ()=> 
+    {const criarTarefa = (evento)=>{
     evento.preventDefault();
     const lista = document.querySelector("[ data-list]");
     const input = document.querySelector("[ data-form-input]");
@@ -10,10 +11,32 @@ const criarTarefa = (evento)=>{
     const conteudo = `<p class="content">${valor}</p>`
 
     tarefa.innerHTML = conteudo;
-    lista.appendChild(tarefa)
+
+    tarefa.appendChild(BotaoConclui());
+    lista.appendChild(tarefa);
     input.value="";
 };
 
 const novaTarefa = document.querySelector("[data-form-buttom]");
 
 novaTarefa.addEventListener("click",criarTarefa);
+
+
+const BotaoConclui = ()=>{
+    const botaoConclui = document.createElement("button");
+
+    botaoConclui.classList.add("check");
+    botaoConclui.innerText = "concluir";
+    botaoConclui.addEventListener("click", concluirTarefa)
+
+    return botaoConclui;
+}
+
+const concluirTarefa = (evento)=>{
+    const botaoConclui = evento.target;
+
+    const tarefaCompleta = botaoConclui.parentElement;
+
+    tarefaCompleta.classList.toggle("done");
+}
+})();
